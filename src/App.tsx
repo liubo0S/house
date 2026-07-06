83868fc611287100befba0ba841fa01918afc24f
liubo62 <liubo62@baidu.com>
2026-07-06 13:31:26 +0800

    feat: redirect unknown routes to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import BedroomPage from './pages/Bedroom'
 import HomePage from './pages/Home'
 
@@ -7,6 +7,8 @@ export default function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/bedroom" element={<BedroomPage />} />
+      {/* 未知路径回退到首页 */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
